b56e4e28c8b3d49da634116113d4d4681185a19e
dev-araujo <theriancirculo@protonmail.com>
2024-09-04 18:42:12 -0300

    feat: delete product

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -74,6 +74,21 @@ class ProductController {
       status: 200,
     });
   }
+
+  async delete(request: Request, response: Response): Promise<Response> {
+    const id = request.params.id;
+
+    const productRepository = AppDataSource.getRepository(Product);
+
+    try {
+      await productRepository.delete(id);
+      return response.status(204).send({});
+    } catch (error) {
+      return response.status(500).send({
+        error: "Error deleting",
+      });
+    }
+  }
 }
 
 export default new ProductController();
